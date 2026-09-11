aba1709e0e5fcf6a4838145a71254483bfd5c517
Rohan Pandey <rohanpandeyy07@gmail.com>
2026-09-11 22:53:17 +0530

    Time: 9 ms (92.36%) | Memory: 46.9 MB (57.26%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,40 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-
+        
         List<Integer> ans = new ArrayList<>();
-
-        if (p.length() > s.length()) {
-            return ans;
+        if(s.length() == 0 || s == null) return ans ;
+        int[] freq = new int[26];
+        for(char c : p.toCharArray()){
+            freq[c-'a']++;
         }
+        int left = 0 ;
+        int right = 0;
+        int count = p.length();
 
-        int k = p.length();
+        while ( right < s.length()){
+            if(freq[s.charAt(right++) - 'a']-- >= 1) count-- ;
 
-        int[] pFreq = new int[26];
-        int[] winFreq = new int[26];
+            if(count == 0 ) ans.add(left);
 
-        for (int i = 0; i < k; i++) {
-            pFreq[p.charAt(i) - 'a']++;
+            if(right - left == p.length() && freq[s.charAt(left++)-'a']++ >= 0) count++;
         }
-
-        for (int i = 0; i < k; i++) {
-            winFreq[s.charAt(i) - 'a']++;
-        }
-
-        if (Arrays.equals(pFreq, winFreq)) {
-            ans.add(0);
-        }
-
-        for (int i = k; i < s.length(); i++) {
-
-            winFreq[s.charAt(i - k) - 'a']--;
-
-            winFreq[s.charAt(i) - 'a']++;
-
-            if (Arrays.equals(pFreq, winFreq)) {
-                ans.add(i - k + 1);
-            }
-        }
-
-        return ans;
+        return ans ;
     }
 }
